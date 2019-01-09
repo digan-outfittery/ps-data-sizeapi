da09b4de538c585dea9b21eb68fc9dcb5c333dae
conor-digan <conordigan@gmail.com>
2019-01-09 19:23:36 +0100

    Changed MLDB table definition

--- sizemodel/resources/create_tables.sql
+++ sizemodel/resources/create_tables.sql
@@ -10,7 +10,7 @@ create table ml.atl_sizemodel_cust_sizes
 drop table if exists ml.atl_sizemodel_article_sizes;
 create table ml.atl_sizemodel_article_sizes
 (
-	article_id int,
+	article_id varchar(100),
 	model_timestamp timestamp,
 	size_object jsonb
 );
